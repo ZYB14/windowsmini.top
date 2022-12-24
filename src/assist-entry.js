d6858acae9a75912b625e13ab56e4117d8c2168b
FSTYYDS <116952494+FSTYYDS@users.noreply.github.com>
2022-12-24 17:58:38 +0800

    Delete src directory

--- src/assist-entry.js
+++
@@ -1,5 +0,0 @@
-import Assist from './assist.js';
-import config from './config.json';
-
-const AssistEntry = new Assist(config)
-export default AssistEntry;
--- src/modules/hack.js
+++
@@ -1,9 +0,0 @@
-
-/*
-ie10 不支持 location.origin，以下是hack方法
-*/
-
-if (!window.location.origin) {
-    window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
-}
-
